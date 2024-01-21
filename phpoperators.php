69cc58bc39eb2572d96cbc59dd5387a090b3a70c
kevinmangukiya2112 <mangukiyakevin2112@gmail.com>
2024-01-21 11:41:44 +0530

    practice sheet

--- phpoperators.php
+++ phpoperators.php
@@ -2,8 +2,8 @@
 <body>
 <?php
 // 1. Arithmetic Operators:
-     $a=10;
-     $b=23;
+    //$a=10;
+    //$b=23;
    //1. Addition:print_r(
     // print_r($a + $b);
     // echo"<br>";
@@ -78,12 +78,16 @@
 
 // Logical Operators:
     //21. Logical AND:
-    //($a && $b)
-    //($a and $b)
+    //var_dump($a && $b);
+    //echo"<br>";
+    //var_dump($a and $b);
+    //echo"<br>";
     //22. Logical OR:
-    //($a || $b)
-    //23. Logical NOT:
-    //(!$a)
+    //var_dump($a || $b);
+    //echo"<br>";
+    //23. Logical NOT:v
+    //var_dump(!$a);
+    //echo"<br>";
 
 // increment and decrement operator 
     //24. Increment:
@@ -91,23 +95,112 @@
     // echo"<br>";
     // print_r($a++);  //postincrement 
     // echo"<br>";
-    //25. Decrement:
-    // print_r(--$b);  //predecrement     
+    // //25. Decrement:
+    // print_r(--$a);  //predecrement     
     // echo"<br>";  
-    // print_r($b--);  //postdecrement
+    // print_r($a--);  //postdecrement
 
 // String Operators:
     //26. Concatenation:
-    print_r($str. $str1);
-    echo"<br>";
+    // print_r($str. $str1);
+    // echo"<br>";
     //27. Concatenation Assignment:
-    $st="$str $str1";   //concate and than assign to another variable 
-    print_r($st);
-    echo"<br>";
-    print_r($str.=$str1);
-    
+    // $st="$str $str1";   //concate and than assign to another variable 
+    // print_r($st);
+    // echo"<br>";
+    // print_r($str.=$str1);
 
+// Array Operators:
+    //$arr=array(1,2,3,"kevin");
+    //$arr1=array("mangukiya","s",3,5,6);
+    //echo"<br>";
+    //28. Union:
+    //print_r($arr + $arr1);
+    //echo"<br>";
+    //29. Equality:
+    //var_dump($arr == $arr1);
+    //echo"<br>";
+    //30. Identity:
+    //var_dump($arr === $arr1);
+    //echo"<br>";
+    //31. Inequality:
+    //var_dump($arr != $arr1);
+    //echo"<br>";
+    //var_dump($arr <> $arr1);
+    //echo"<br>";
+    //32. Non-identity:
+    //var_dump($arr !== $arr1);
+    //echo"<br>";
 
+// Conditional (Ternary) Operator: --> reduce if else and still gives same ans 
+    //33. Ternary:
+    // $con=10;
+    // $arg1="kevin";
+    // $arg2="mangukiya";
+    // print_r(($con>5)? $arg1:$arg2);   // it will print arg 1
+    // echo"<br>";
+    // print_r(($con>=10)? $arg1:$arg2); // it will print arg 1
+    // echo"<br>";
+    // print_r(($con>15)? $arg1:$arg2);  // it will print arg 2
+    // echo"<br>";
+
+// Null Coalescing Operator:
+    //34. Null Coalescing:
+    // $a=NULL;
+    // $b="10";
+    // print_r($a ?? $b);
+    // echo"<br>";
+    // print_r($a ?: $b);
+
+// Type Operators:
+    //35. Type Of:
+    // $int=10;
+    // $flt=12.5;
+    // $str="kevin";
+    // $bool=TRUE;
+    // $arr=array(1,2,3,"kevin");
+    // $nul=NULL;
+    // print_r(typeof($int));
+    // echo"<br>";
+    // print_r(gettype($int));
+    // echo"<br>";
+    // print_r(typeof($flt));
+    // echo"<br>";
+    // print_r(gettype($flt));
+    // echo"<br>";
+    // print_r(typeof($str));
+    // echo"<br>";
+    // print_r(gettype($str));
+    // echo"<br>";
+    // print_r(typeof($bool));
+    // echo"<br>";
+    // print_r(gettype($bool));
+    // echo"<br>";
+    // print_r(typeof($arr));
+    // echo"<br>";
+    // print_r(gettype($arr));
+    // echo"<br>";
+    // print_r(typeof($null));
+    // echo"<br>";
+    // print_r(gettype($nul));
+
+// instance of 
+    // class ksm{}
+    // class MyClass {}
+    // class AnotherClass extends MyClass{}
+    // $obj = new AnotherClass();
+
+    // if($obj instanceof AnotherClass) {
+    //   echo "The object is AnotherClass <br>";
+    // }
+
+    // // The object is also an instance of the class it is derived from
+    // if($obj instanceof MyClass) {
+    //   echo "The object is MyClass<br>";
+    // }
+    // if($obj instanceof ksm) {
+    //     echo "The object is MyClass<br>";
+    //   }
 
 ?>
 </body>
